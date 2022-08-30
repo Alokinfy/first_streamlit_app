8b506b9074555ad5fe38864dc0c2f812b4c55711
Alok Pise <112507044+Alokinfy@users.noreply.github.com>
2022-08-30 12:46:14 -0500

    Update streamlit_app.py
    Removed Chicken Sandwich and added Avacado Toast with Build your own Smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner!')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Bluberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Booiled Free-Ranged Egg')
-streamlit.text('Chicken Sandwich')
+streamlit.text('🥣 Omega 3 and Bluberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Booiled Free-Ranged Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
