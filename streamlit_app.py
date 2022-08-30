d378c1035028f2479fd3d2c64eba1de8d8c46c8d
Alok Pise <112507044+Alokinfy@users.noreply.github.com>
2022-08-30 12:48:34 -0500

    Update streamlit_app.py
    added emoji to breakfast menu header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner!')
-streamlit.header('Breakfast Menu')
+streamlit.header('🍔 Breakfast Menu 🍔')
 streamlit.text('🥣 Omega 3 and Bluberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Booiled Free-Ranged Egg')
