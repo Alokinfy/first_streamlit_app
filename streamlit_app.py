21f05f57730b2175788b326b67b1f12a457823c4
Alok Pise <112507044+Alokinfy@users.noreply.github.com>
2022-08-30 12:39:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner!')
 streamlit.header('Breakfast Menu')
-Streamlit.text('Omega 3 and Bluberry Oatmeal')
+streamlit.text('Omega 3 and Bluberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Booiled Free-Ranged Egg')
